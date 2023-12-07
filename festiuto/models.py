ec23eee20542bdd3952dce2fc9e8c9303a164e4e
Yohann Loiseau <yohann.loiseau@etu.univ-orleans.fr>
2023-12-07 11:52:41 +0100

    modele fonctionnel

--- festiuto/models.py
+++ festiuto/models.py
@@ -12,31 +12,39 @@ class Artiste(db.Model):
     groupe = db.relationship('Groupe', backref='artistes')
 
     def __repr__(self):
-        return f"<Artiste ida={self.ida} noma={self.noma} prenoma={self.prenoma} dateNaissA={self.dateNaissA} idG={self.idG}>"
+        return f"<Artiste ida={self.idA} noma={self.nomA} prenoma={self.prenomA} dateNaissA={self.dateNaissA} idG={self.idG}>"
 
+class TypeBillet(db.Model):
+    idTb = db.Column(db.Integer, primary_key=True)
+    nomB = db.Column(db.String(50))
+    prix = db.Column(db.DECIMAL(5, 2))
+    duree = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<TypeBillet idTb={self.idTb} nomB={self.nomB} prix={self.prix} duree={self.duree}>"
 
 class Billet(db.Model):
     idB = db.Column(db.Integer, primary_key=True)
     dateDebutValidite = db.Column(db.Date)
-    idTb = db.Column(db.Integer, db.ForeignKey('typebillet.idTb'), nullable=False)
-    idV = db.Column(db.Integer, db.ForeignKey('visiteur.idVisiteur'), nullable=False)
+    idV = db.Column(db.Integer, db.ForeignKey('visiteur.idV'), nullable=False)
+    idTb = db.Column(db.Integer, db.ForeignKey('typeBillet.idTb'), nullable=False)
 
-    typeBillet = db.relationship('idTb', backref='billets')
+    typeBillet = db.relationship('TypeBillet', backref='billets')
     visiteur = db.relationship('Visiteur', backref='billets')
 
     def __repr__(self):
         return f"<Billet idB={self.idB} dateDebutValidite={self.dateDebutValidite} idTb={self.idTb} idV={self.idV}>"
     
 class EtreSousStyle(db.Model):
-    idS_1 = db.Column(db.Integer, db.ForeignKey('style.idStyle'), primary_key=True, nullable=False)
-    idS_2 = db.Column(db.Integer, db.ForeignKey('style.idStyle'), primary_key=True, nullable=False)
+    idS_1 = db.Column(db.Integer, db.ForeignKey('style.idS'), primary_key=True, nullable=False)
+    idS_2 = db.Column(db.Integer, db.ForeignKey('style.idS'), primary_key=True, nullable=False)
 
     s1 = db.relationship('Style', backref='sous_styles1')
     s2 = db.relationship('Style', backref='sous_styles2')
 
 class Favoris(db.Model):
-    idG = db.Column(db.String(42), db.ForeignKey('groupe.idGroupe'), primary_key=True, nullable=False)
-    idV = db.Column(db.Integer, db.ForeignKey('visiteur.idVisiteur'), primary_key=True, nullable=False)
+    idG = db.Column(db.String(42), db.ForeignKey('groupe.idG'), primary_key=True, nullable=False)
+    idV = db.Column(db.Integer, db.ForeignKey('visiteur.idV'), primary_key=True, nullable=False)
 
     groupe = db.relationship('Groupe', backref='favoris')
     visiteur = db.relationship('Visiteur', backref='favoris')
@@ -58,17 +66,17 @@ class Hebergement(db.Model):
     nbPlaces = db.Column(db.Integer)
 
     def __repr__(self):
-        return f"<Hebergement idH={self.idH} nomHebergement={self.nomHebergement} addresse={self.addresse} nbPlaces={self.nbPlaces}>"
+        return f"<Hebergement idH={self.idH} nomHebergement={self.nomH} addresse={self.addresse} nbPlaces={self.nbPlaces}>"
 
 
 class Heberge(db.Model):
-    idG = db.Column(db.String(42), db.ForeignKey('groupe.idGroupe'), primary_key=True, nullable=False)
-    idH = db.Column(db.Integer, db.ForeignKey('hebergement.idHebergement'), primary_key=True, nullable=False)
+    idG = db.Column(db.String(42), db.ForeignKey('groupe.idG'), primary_key=True, nullable=False)
+    idH = db.Column(db.Integer, db.ForeignKey('hebergement.idH'), primary_key=True, nullable=False)
     dateDebut = db.Column(db.Date)
     dateFin = db.Column(db.Date)
 
     groupe = db.relationship('Groupe', backref='herberge')
-    hbergement = db.relationship('Herbegement', backref='heberge')
+    hebergement = db.relationship('Herbegement', backref='heberge')
 
     def __repr__(self):
         return f"<Heberge idG={self.idG} idH={self.idH} dateDebut={self.dateDebut} dateFin={self.dateFin}>"
@@ -84,13 +92,16 @@ class Instrument(db.Model):
 
 class LienRS(db.Model):
     idG = db.Column(db.String(42), primary_key=True, nullable=False)
-    idR = db.Column(db.Integer, db.ForeignKey('reseausocial.idReseauSocial'), primary_key=True, nullable=False)
+    idRs = db.Column(db.Integer, db.ForeignKey('reseausocial.idRs'), primary_key=True, nullable=False)
     pos = db.Column(db.Integer)
     pseudo = db.Column(db.String(80))
     urlReseau = db.Column(db.String(200))
 
+    groupe = db.relationship('Groupe', backref='lienrs')
+    reseau = db.relationship('ReseauSocial', backref='lienrs')
+
     def __repr__(self):
-        return f"<LienRS idG={self.idG} idRs={self.idRs} pos={self.pos} pseudo={self.pseudo} urlReseau={self.urlReseau}>"
+        return f"<LienRS idG={self.idG} idRs={self.idR} pos={self.pos} pseudo={self.pseudo} urlReseau={self.urlReseau}>"
 
 
 class Lieu(db.Model):
@@ -105,7 +116,7 @@ class Lieu(db.Model):
 class Photo(db.Model):
     idPh = db.Column(db.Integer, primary_key=True)
     urlPh = db.Column(db.String(200))
-    idG = db.Column(db.String(42), db.ForeignKey('groupe.idGroupe'), nullable=False)
+    idG = db.Column(db.String(42), db.ForeignKey('groupe.idG'), nullable=False)
     pos = db.Column(db.Integer)
 
     groupe = db.relationship('Groupe', backref='photo')
@@ -123,8 +134,8 @@ class ReseauSocial(db.Model):
         return f"<ReseauSocial idRs={self.idRs} nomReseau={self.nomReseau} urlLogoReseau={self.urlLogoReseau}>"
     
 class Jouer(db.Model):
-    idI = db.Column(db.Integer, db.ForeignKey('instrument.idInstrument'), primary_key=True, nullable=False)
-    idA = db.Column(db.Integer, db.ForeignKey('artiste.idArtiste'), primary_key=True, nullable=False)
+    idI = db.Column(db.Integer, db.ForeignKey('instrument.idI'), primary_key=True, nullable=False)
+    idA = db.Column(db.Integer, db.ForeignKey('artiste.idA'), primary_key=True, nullable=False)
 
     instrument = db.relationship('Instrument', backref='joueurs')
     artiste = db.relationship('Artiste', backref='instruments')
@@ -138,18 +149,11 @@ class Style(db.Model):
         return f"<Style idS={self.idS} nomS={self.nomS}>"
 
 
-class TypeBillet(db.Model):
-    idTb = db.Column(db.Integer, primary_key=True)
-    nomB = db.Column(db.String(50))
-    prix = db.Column(db.DECIMAL(5, 2))
-    duree = db.Column(db.Integer)
 
-    def __repr__(self):
-        return f"<TypeBillet idTb={self.idTb} nomB={self.nomB} prix={self.prix} duree={self.duree}>"
     
 class Posseder(db.Model):
-    idG = db.Column(db.String(42), db.ForeignKey('groupe.idGroupe'), primary_key=True, nullable=False)
-    idS = db.Column(db.Integer, db.ForeignKey('style.idStyle'), primary_key=True, nullable=False)
+    idG = db.Column(db.String(42), db.ForeignKey('groupe.idG'), primary_key=True, nullable=False)
+    idS = db.Column(db.Integer, db.ForeignKey('style.idS'), primary_key=True, nullable=False)
 
     groupe = db.relationship('Groupe', backref='styles')
     style = db.relationship('Style', backref='groupes')
@@ -158,7 +162,7 @@ class Posseder(db.Model):
 class Video(db.Model):
     idVideo = db.Column(db.Integer, primary_key=True)
     urlVideo = db.Column(db.String(200))
-    idG = db.Column(db.String(42), db.ForeignKey('groupe.idGroupe'), nullable=False)
+    idG = db.Column(db.String(42), db.ForeignKey('groupe.idG'), nullable=False)
     pos = db.Column(db.Integer)
 
     groupe = db.relationship('Groupe', backref='video')
@@ -167,8 +171,8 @@ class Video(db.Model):
         return f"<Video idVideo={self.idVideo} urlVideo={self.urlVideo} idG={self.idG} pos={self.pos}>"
     
 class SInscrit(db.Model):
-    idEv = db.Column(db.Integer, db.ForeignKey('evenement.idEvenement'), primary_key=True, nullable=False)
-    idV = db.Column(db.Integer, db.ForeignKey('visiteur.idVisiteur'), primary_key=True, nullable=False)
+    idEv = db.Column(db.Integer, db.ForeignKey('evenement.idE'), primary_key=True, nullable=False)
+    idV = db.Column(db.Integer, db.ForeignKey('visiteur.idV'), primary_key=True, nullable=False)
 
     evenement = db.relationship('Evenement', backref='inscrits')
     visiteur = db.relationship('Visiteur', backref='inscriptions')
@@ -187,3 +191,24 @@ class Visiteur(db.Model):
     def __repr__(self):
         return f"<Visiteur idV={self.idV} nomV={self.nomV} prenomV={self.prenomV} dateNaissV={self.dateNaissV} " \
                f"numtel={self.numtel} email={self.email} motdepasse={self.motdepasse} admin={self.admin}>"
+    
+class Evenement(db.Model):
+    idE = db.Column(db.Integer, primary_key=True)
+    typeE = db.Column(db.String(80))
+    descrE = db.Column(db.String(200))
+    tempsMontage = db.Column(db.Time)
+    tempsDemontage = db.Column(db.Time)
+    gratuit = db.Column(db.Boolean)
+    dateDebut = db.Column(db.DateTime)
+    dateFin = db.Column(db.DateTime)
+    idG = db.Column(db.String(42), db.ForeignKey('groupe.idG'), nullable=False)
+    idL = db.Column(db.Integer, db.ForeignKey('lieu.idL'), nullable=False)
+
+    groupe = db.relationship('Groupe', backref='evenements')
+    lieu = db.relationship('Lieu', backref='evenements')
+
+    def __repr__(self):
+        return f"<Evenement idEv={self.idEv} typeEv={self.typeEv} descrEv={self.descrEv} " \
+               f"tempsMontage={self.tempsMontage} tempsDemontage={self.tempsDemontage} " \
+               f"gratuit={self.gratuit} dateDebut={self.dateDebut} dateFin={self.dateFin} " \
+               f"idG={self.idG} idL={self.idL}>"
