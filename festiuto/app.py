ebe1395f6725d6911299a9d4a986ff7630fc7cc7
marintremine <extrayer.dev@gmail.com>
2024-01-07 22:46:59 +0100

    update 07/01/2024

--- festiuto/app.py
+++ festiuto/app.py
@@ -10,6 +10,7 @@ from flask_wtf.csrf import CSRFProtect
 
 from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
+from sqlalchemy import inspect
 
 
 
@@ -20,8 +21,8 @@ htmx = HTMX(app)
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://tremine:tremine@servinfo-maria:3306/DBtremine'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://root:root@localhost:3306/festiuto'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://root@localhost:3306/festiuto'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://root:root@localhost:3306/festiuto'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://root@localhost:3306/festiuto'
 app.config['SECRET_KEY'] = 'iMQWPgaEP2WQQUxPvKiYiZoP5jaP5RdzGoE4msqtGFTJgSVKTwVH3SEUGsjRRTkFZMKqXKmCsAaEWbdjWJEb8ip2rNi4hCKezTxe5VVXfiAgDfYzdLRAEqf3dou8gGwr'
 
 
@@ -39,6 +40,8 @@ def admin():
     Initialisation de l'interface d'administration
     """
     class myModelView(ModelView):
+        column_display_pk = True # optional, but I like to see the IDs in the list
+        column_hide_backrefs = False
         def is_accessible(self):
             return current_user.is_authenticated and current_user.admin
         def inaccessible_callback(self, name, **kwargs):
@@ -46,6 +49,56 @@ def admin():
             flash("Vous n'avez pas accès à cette page")
             return redirect(url_for('connexion'))
         
+    class billetView(myModelView):
+        column_list = ('idB','dateDebutValidite', 'idV', 'idTb')
+        form_columns = ('idB','dateDebutValidite', 'idV', 'idTb')
+
+    class hebergeView(myModelView):
+        column_list = ('idG','idH', 'dateDebut', 'dateFin')
+        form_columns = ('idG','idH', 'dateDebut', 'dateFin')
+
+    class lienRSView(myModelView):
+        column_list = ('idG','idRs','pos','pseudo', 'urlReseau')
+        form_columns = ('idG','idRs','pos','pseudo', 'urlReseau')
+
+    class favorisView(myModelView):
+        column_list = ('idG','idV')
+        form_columns = ('idG','idV')
+
+    class photoView(myModelView):
+        column_list = ('idPh','idG','urlPh', 'pos')
+        form_columns =  ('idPh','idG','urlPh', 'pos')
+
+    class s_inscritView(myModelView):
+        column_list = ('idEv','idV')
+        form_columns = ('idEv','idV')
+        
+    class videoView(myModelView):
+        column_list = ('idVideo','idG','urlVideo', 'pos')
+        form_columns = ('idVideo','idG','urlVideo', 'pos')
+
+    class evenementView(myModelView):
+        column_list = ('idEv','typeEv','descrEv','tempsMontage','tempsDemontage','gratuit','dateDebut','dateFin','idG','idL')
+        form_columns = ('idEv','typeEv','descrEv','tempsMontage','tempsDemontage','gratuit','dateDebut','dateFin','idG','idL')
+
+    class artisteView(myModelView):
+        column_list = ('idA','nomA','prenomA','dateNaissA','idG')
+        form_columns = ('idA','nomA','prenomA','dateNaissA','idG')
+
+    class jouerView(myModelView):
+        column_list = ('idI','idA')
+        form_columns = ('idI','idA')
+
+    class possederView(myModelView):
+        column_list = ('idG','idS')
+        form_columns = ('idG','idS')
+
+    class styleView(myModelView):
+        column_list = ('idS_1','idS_2','nomS')
+        form_columns = ('idS_1','idS_2','nomS')
+
+    
+
     class myAdminIndexView(AdminIndexView):
         @expose('/')
         def index(self):
@@ -54,31 +107,32 @@ def admin():
             else:
                 flash("Vous n'avez pas accès à cette page")
                 return redirect(url_for('connexion'))
-        
 
     from .models import Visiteur, Artiste, Groupe, TypeBillet, Billet, Favoris, Hebergement, Heberge, Instrument, LienRS, Lieu, Photo, ReseauSocial, Jouer, Style, SInscrit, Posseder, Video, Evenement
-    admin = Admin(app, name='Tableau de bord',index_view=myAdminIndexView, template_mode='bootstrap3')
+
+    app.config['FLASK_ADMIN_SWATCH'] = 'Darkly'
+    admin = Admin(app, name='Tableau de bord',index_view=myAdminIndexView(), template_mode='bootstrap3')
 
 
     admin.add_view(myModelView(Visiteur, db.session))
-    admin.add_view(myModelView(Artiste, db.session))
+    admin.add_view(artisteView(Artiste, db.session))
     admin.add_view(myModelView(Groupe, db.session))
     admin.add_view(myModelView(TypeBillet, db.session))
-    admin.add_view(myModelView(Billet, db.session))
-    admin.add_view(myModelView(Favoris, db.session))
+    admin.add_view(billetView(Billet, db.session))
+    admin.add_view(favorisView(Favoris, db.session))
     admin.add_view(myModelView(Hebergement, db.session))
-    admin.add_view(myModelView(Heberge, db.session))
+    admin.add_view(hebergeView(Heberge, db.session))
     admin.add_view(myModelView(Instrument, db.session))
-    admin.add_view(myModelView(LienRS, db.session))
+    admin.add_view(lienRSView(LienRS, db.session))
     admin.add_view(myModelView(Lieu, db.session))
-    admin.add_view(myModelView(Photo, db.session))
+    admin.add_view(photoView(Photo, db.session))
     admin.add_view(myModelView(ReseauSocial, db.session))
-    admin.add_view(myModelView(Jouer, db.session))
-    admin.add_view(myModelView(Style, db.session))
-    admin.add_view(myModelView(SInscrit, db.session))
-    admin.add_view(myModelView(Posseder, db.session))
-    admin.add_view(myModelView(Video, db.session))
-    admin.add_view(myModelView(Evenement, db.session))
+    admin.add_view(jouerView(Jouer, db.session))
+    admin.add_view(styleView(Style, db.session))
+    admin.add_view(s_inscritView(SInscrit, db.session))
+    admin.add_view(possederView(Posseder, db.session))
+    admin.add_view(videoView(Video, db.session))
+    admin.add_view(evenementView(Evenement, db.session))
 
 admin()
 @login_manager.unauthorized_handler
